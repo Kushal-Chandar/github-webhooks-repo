9f81f1ca02c760a4fdee41e6c9a15f77f7960f2a
kushal-chandar <kushalchandariit@gmail.com>
2024-09-20 13:13:21 +0530

    feat: logging

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, current_app
 from .extensions import events_collection
 from pymongo import errors
 from datetime import datetime, timedelta, timezone
@@ -8,13 +8,17 @@ root = Blueprint("endpoints", __name__, url_prefix="/")
 
 @root.route("/events", methods=["GET"])
 def get_events():
-    """
-    Fetch a list of events with pagination.
-    """
+    current_app.logger.info("GET /events endpoint called")
+
     try:
         page = int(request.args.get("page", 1))
         limit = int(request.args.get("limit", 10))
         skip = (page - 1) * limit
+
+        current_app.logger.debug(
+            f"Pagination parameters - Page: {page}, Limit: {limit}, Skip: {skip}"
+        )
+
         events = (
             events_collection.find(
                 {"timestamp": {"$gte": datetime.utcnow() - timedelta(seconds=15)}}
@@ -23,8 +27,14 @@ def get_events():
             .skip(skip)
             .limit(limit)
         )
+
         total_count = events_collection.count_documents({})
         total_pages = (total_count // limit) + (1 if total_count % limit > 0 else 0)
+
+        current_app.logger.debug(
+            f"Total events: {total_count}, Total pages: {total_pages}"
+        )
+
         events_data = [
             {
                 **event,
@@ -36,6 +46,7 @@ def get_events():
             for event in events
         ]
 
+        current_app.logger.info(f"{len(events_data)} events returned for page {page}")
         return jsonify(
             {
                 "events": events_data,
@@ -46,13 +57,16 @@ def get_events():
         )
 
     except errors.PyMongoError as e:
+        current_app.logger.error(f"Database error occurred: {e}")
         return jsonify({"status": "failure", "message": "Database error"}), 500
     except ValueError as e:
+        current_app.logger.warning(f"Invalid pagination parameters: {e}")
         return (
             jsonify({"status": "failure", "message": "Invalid pagination parameters"}),
             400,
         )
     except Exception as e:
+        current_app.logger.exception(f"Unexpected error occurred: {e}")
         return (
             jsonify({"status": "failure", "message": "An unexpected error occurred"}),
             500,
@@ -61,4 +75,5 @@ def get_events():
 
 @root.route("/")
 def index():
+    current_app.logger.info("GET / endpoint called")
     return render_template("index.html")
